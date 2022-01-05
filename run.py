a106f3b9652e2411e78b5ed39f30bdc653086d05
jtlaune <jtlaune@gmail.com>
2022-01-04 23:10:44 -0500

    First tests with params by aext and muext

--- run.py
+++ run.py
@@ -487,18 +487,18 @@ def run_tp(h, j, mup, ap, a0, ep, e0, g0, Tm, Te, T, suptitle,
             data = np.load(os.path.join(dirname, filename))
             teval  = data["teval"]
             theta = data["thetap"]
-            a1     = data["a1"]
-            a2     = data["a2"]
-            e1     = data["e1"]
-            e2     = data["e2"]
-            g1     = data["g1"]
-            g2     = data["g2"]
-            L1     = data["L1"]
-            L2     = data["L2"]
-            x1     = data["x1"]
-            y1     = data["y1"]
-            x2     = data["x2"]
-            y2     = data["y2"]
+            a1 = data["a1"]
+            a2 = data["a2"]
+            e1 = data["e1"]
+            e2 = data["e2"]
+            g1 = data["g1"]
+            g2 = data["g2"]
+            L1 = data["L1"]
+            L2 = data["L2"]
+            x1 = data["x1"]
+            y1 = data["y1"]
+            x2 = data["x2"]
+            y2 = data["y2"]
 
     else:
         sim = tp_intH(j, mup, ep, e0, ap, g0, a0, lambda0)
@@ -1097,6 +1097,7 @@ def run_compmass(h, j, mu1, q, a0, alpha2_0, e1_0, e2_0, g1_0, g2_0,
 
     return fig
 
+
 class run_compmass_set:
     def __init__(self, verbose=False, overwrite=False, secular=True, method="RK45"):
         self.verbose   = verbose
@@ -1155,3 +1156,291 @@ class run_compmass_set:
                      e1d=e1d, e2d=e2d, overwrite=self.overwrite,
                      cutoff=cutoff, method=self.method,
                      Te_func=Te_func)
+
+
+class comp_mass_omeff(comp_mass_intH):
+    def __init__(self, j, mu1, q, a0, Tm1, Tm2, Te1, Te2, e1d=None,
+                 e2d=None, cutoff=np.infty, Te_func=False, aext=0., muext=0.):
+        super().__init__(self, j, mu1, q, a0, Tm1, Tm2, Te1, Te2, e1d=None,
+                 e2d=None, cutoff=np.infty, Te_func=False)
+        self.aext
+        self.muext
+    def H4dofsec(self, t, Y):
+        (thetadot, L1dot, L2dot,
+         x1dot, y1dot, x2dot, y2dot) = super().H4dofsec(t,Y)
+
+        aext = self.aext
+        muext = self.muext
+
+        if self.perturb:
+################### old stuff
+            # 7 variables
+            theta = Y[0]
+            L1 = Y[1]
+            L2 = Y[2]
+            x1 = Y[3]
+            y1 = Y[4]
+            x2 = Y[5]
+            y2 = Y[6]
+
+            g1 = np.arctan2(y1, x1)
+            G1 = sqrt(x1 * x1 + y1 * y1)
+            g2 = np.arctan2(y2, x2)
+            G2 = sqrt(x2 * x2 + y2 * y2)
+
+            j = self.j
+            mu2 = self.mu1 / self.q
+
+            e1 = np.sqrt(1 - (1 - G1 / L1) ** 2)
+            e2 = np.sqrt(1 - (1 - G2 / L2) ** 2)
+
+            # the Hamiltonian is -(constants) as the internal TP
+            # Hamiltonian, i.e. it matches MD
+            alpha1 = L1 * L1 / self.q / self.q
+            alpha2 = L2 * L2
+            alpha = alpha1 / alpha2
+            theta1 = theta + g1
+            theta2 = theta + g2
+            f1 = -self.A(alpha)
+            f2 = -self.B(alpha)
+            C = self.C(alpha)
+            D = self.D(alpha)
+
+################### new stuff
+
+            om1ext = om1ext_np(self.muext, a1, a2, self.aext)
+            om2ext = om2ext_np(self.muext, a1, a2, self.aext)
+
+            xdot = xdot + self.om_eff * sqrt(G) * sin(g)
+            ydot = ydot - self.om_eff * sqrt(G) * cos(g)
+
+            # see above for the ldot secular force from mup
+            # these are just ldot secular forcing from ext
+            l1dotext = (
+                -self.muext
+                * L1
+                * (a2 * a2 / self.aext / self.aext)
+                * LC.Db(0.5, 0, a1 / self.aext)
+            )
+            l2dotext = (
+                -(a2 * a2 / self.aext / self.aext)
+                * self.muext
+                * LC.Db(0.5, 0, a2 / self.aext)
+            )
+            
+            thetadot = thetadot + (j+1)*l2dotext - j*l1dotext
+
+################### new stuff
+        return(np.array([thetadot, L1dot, L2dot, x1dot,
+                        y1dot, x2dot, y2dot]))
+
+
+def run_compmass_omeff(h, j, mu1, q, a0, alpha2_0, e1_0, e2_0, g1_0,
+                       g2_0, Tm1, Tm2, Te1, Te2, T, suptitle, dirname,
+                       filename, figname, paramsname, verbose, tscale,
+                       secular, e1d, e2d, overwrite, cutoff, method,
+                       Te_func, muext, aext):
+    print(method)
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname, exist_ok=True)
+    if os.path.exists(os.path.join(dirname, filename)):
+        if overwrite:
+            sim = comp_mass_omeff(j, mu1, q, a0, Tm1, Tm2, Te1, Te2,
+                                  e1d, e2d, cutoff, Te_func, aext, muext)
+            (teval, theta, a1, a2, e1, e2,
+            g1, g2, L1, L2, x1, y1, x2, y2) = sim.int_Hsec(T, 1e-9,
+                                                           alpha2_0, e1_0,
+                                                           e2_0,g1_0, g2_0,
+                                                           verbose=verbose,
+                                                           secular=secular,
+                                                           method=method)
+            np.savez(
+                os.path.join(dirname, filename),
+                teval=teval,
+                thetap=theta,
+                a1=a1,
+                a2=a2,
+                e1=e1,
+                e2=e2,
+                g1=g1,
+                g2=g2,
+                L1=L1,
+                L2=L2,
+                x1=x1,
+                y1=y1,
+                x2=x2,
+                y2=y2,
+            )
+        else:
+            data = np.load(os.path.join(dirname, filename))
+            teval  = data["teval"]
+            theta = data["thetap"]
+            a1     = data["a1"]
+            a2     = data["a2"]
+            e1     = data["e1"]
+            e2     = data["e2"]
+            g1     = data["g1"]
+            g2     = data["g2"]
+            L1     = data["L1"]
+            L2     = data["L2"]
+            x1     = data["x1"]
+            y1     = data["y1"]
+            x2     = data["x2"]
+            y2     = data["y2"]
+            
+    else:
+        sim = comp_mass_omeff(j, mu1, q, a0, Tm1, Tm2, Te1, Te2,
+                              e1d, e2d, cutoff, Te_func, aext, muext)
+        (teval, theta, a1, a2, e1, e2,
+        g1, g2, L1, L2, x1, y1, x2, y2) = sim.int_Hsec(T, 1e-9,
+                                                       alpha2_0, e1_0,
+                                                       e2_0,g1_0, g2_0,
+                                                       verbose=verbose,
+                                                       secular=secular,
+                                                       method=method)
+        np.savez(
+            os.path.join(dirname, filename),
+            teval=teval,
+            thetap=theta,
+            a1=a1,
+            a2=a2,
+            e1=e1,
+            e2=e2,
+            g1=g1,
+            g2=g2,
+            L1=L1,
+            L2=L2,
+            x1=x1,
+            y1=y1,
+            x2=x2,
+            y2=y2,
+        )
+
+
+    theta1 = (theta + g1) % (2 * np.pi)
+    theta2 = (theta + g2) % (2 * np.pi)
+    alpha = a1 / a2
+    period_ratio = (alpha) ** (1.5)
+    pnom = j / (j + 1)
+    pdiff = period_ratio - pnom
+
+    f1 = A(alpha, j)
+    f2 = B(alpha, j)
+    barg1 = np.arctan2(e2*np.sin(g2), e2*np.cos(g2) + f2*e1/f1)
+    barg2 = np.arctan2(e1*np.sin(g1), e1*np.cos(g1) + f1*e2/f2)
+
+    bartheta1 = (theta + barg1) % (2*np.pi)
+    bartheta2 = (theta + barg2) % (2*np.pi)
+    # from the reducing rotation (Henrard et al 1986)
+    hattheta1 = np.arctan2(e1*sin(theta1) + f2/f1*e2*sin(theta2),
+                          e1*cos(theta1) + f2/f1*e2*cos(theta2))
+    hattheta2 = np.arctan2(e2*sin(theta2) + f1/f2*e1*sin(theta1),
+                           e2*cos(theta2) + f1/f2*e1*cos(theta1))
+    fig, ax = plt.subplots(5, 2, figsize=(10, 22))
+    fontsize = 24
+
+    # make a quick diagnostic plot
+    plotsim(
+        fig,
+        ax,
+        teval,
+        suptitle,
+        tscale,
+        fontsize,
+        (r"$a_1$", a1),
+        (r"$\varpi_1-\varpi_2$", g1-g2),
+        (r"$e_1$", e1),
+        (r"$e_2$", e2),
+        (r"$\theta_1$", theta1),
+        (r"$\theta_2$", theta2),
+        (r"$\overline{\theta}_1$", bartheta1),
+        (r"$\overline{\theta}_2$", bartheta2),
+        (r"$\hat{\theta}_1$", hattheta1),
+        (r"$\hat{\theta}_2$", hattheta2),
+        yfigupper=0.99,
+    )
+
+    ax[0, 0].scatter(teval / tscale, a2, s=2)
+    axp = ax[0, 0].twinx()
+    axp.scatter(teval/tscale, period_ratio, s=2, c="r")
+    axp.set_ylabel(r"$P_1/P_2$", fontsize=fontsize)
+    axp.tick_params(labelsize=fontsize)
+    fig.subplots_adjust(wspace=0.6)
+
+    fig.savefig(os.path.join(dirname, figname))
+
+    commit = subprocess.check_output(
+        ['git', 'rev-parse', '--short', 'HEAD']).decode().strip()
+
+    variables = [j,q,mu1,a0,alpha2_0,Tm1,Tm2,Te1,Te2,T]
+    variable_names = ["j","q","mu1","a0",
+                      "alpha2_0","Tm1","Tm2","Te1","Te2","T"]
+
+    with open(os.path.join(dirname, paramsname), "w+") as f:
+        f.write("".join(["{} = {}\n".format(name, variable) for 
+                         name, variable in zip(variable_names, variables)]))
+        if not secular:
+            f.write("\nSECULAR TERMS OFF\n")
+        f.write("\ncommit {}".format(commit))
+
+    return fig
+
+
+class run_compmass_set_omeff(run_compmass_set):
+    def __init__(self, verbose=False, overwrite=False, secular=True, method="RK45"):
+        self.verbose   = verbose
+        self.overwrite = overwrite
+        self.secular   = secular
+        self.method    = method
+    def __call__(self, params):
+        h = np.float64(params[0])
+        j = np.float64(params[1])
+        a0 = np.float64(params[2])
+        q = np.float64(params[3])
+        mu1 = np.float64(params[4])
+        T = np.float64(params[5])
+
+        Te_func = int(float(params[18]))
+        if Te_func:
+            Te1 = params[6]
+            Te2 = params[7]
+            Tm1 = params[8]
+            Tm2 = params[9]
+        else:
+            Te1 = np.float64(params[6])
+            Te2 = np.float64(params[7])
+            Tm1 = np.float64(params[8])
+            Tm2 = np.float64(params[9])
+
+        e1_0 = np.float64(params[10])
+        e2_0 = np.float64(params[11])
+        e1d = np.float64(params[12])
+        e2d = np.float64(params[13])
+        alpha2_0 = np.float64(params[14])
+        name = params[15]
+        dirname = params[16]
+        cutoff = np.float64(params[17])
+        g1_0 = np.float64(params[19])
+        g2_0 = np.float64(params[20])
+        muext = np.float64(params[21])
+        aext = np.float64(params[22])
+        filename   = f"{name}.npz"
+        figname    = f"{name}.png"
+        paramsname = f"params-{name}.txt"
+        if Te_func:
+            suptitle = (f"{filename}\n" \
+                        f"T={T:0.1e} q={q} " + r"$\mu_{1}=$ " + f"{mu1:0.2e}\n" \
+                        r"$a_{\rm ext}$ = " + f"{aext:0.3f} " \
+                        r"$\mu_{\rm ext}$ = " + f"{muext:0.3f}")
+        else:
+            suptitle = (f"{filename}\n" \
+                        f"T={T:0.1e} q={q} " + r"$\mu_{1}=$ " + f"{mu1:0.2e}\n" \
+                        f"Tm1={Tm1:0.1e} Te1={Te1:0.1e}\n" \
+                        f"Tm2={Tm2:0.1e} Te2={Te2:0.1e}\n" \
+                        r"$a_{\rm ext}$ = " + f"{aext:0.3f} " \
+                        r"$\mu_{\rm ext}$ = " + f"{muext:0.3f}")
+        run_compmass_omeff(h, j, mu1, q, a0, alpha2_0, e1_0,
+                           e2_0,g1_0, g2_0, Tm1, Tm2, Te1, Te2, T, suptitle,
+                           dirname, filename, figname, paramsname, verbose,
+                           secular, e1d, e2d, overwrite, cutoff, method,
+                           Te_func,muext, aext)
