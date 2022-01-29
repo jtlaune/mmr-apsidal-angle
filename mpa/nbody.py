9e4e4c23aec9259ac5ba3f26614ef7e410280910
jtlaune <jtlaune@gmail.com>
2022-01-28 19:47:11 -0500

    Update

--- mpa/nbody.py
+++ mpa/nbody.py
@@ -1,60 +1,15 @@
 from . import *
 
-
-class NbodyMigTrapSeries(SimSeries):
-    def __call__(self, Nproc=8):
-        # change to series directory
-        if not os.path.exists(self.seriesname):
-            os.mkdir(self.seriesname)
-        os.chdir(self.seriesname)
-        print(os.getcwd())
-
-        N_sims = self.RUN_PARAMS.shape[0]
-
-        overwrite = not self.load
-        integrate = CompmassSetOmeff(verbose=True,
-                                       overwrite=overwrite,
-                                       secular=True,
-                                       method="RK45")
-        np.savez("RUN_PARAMS", self.RUN_PARAMS)
-        print(self.RUN_PARAMS)
-        print(f"Running {N_sims} simulations...")
-        
-        with Pool(processes=min(Nproc, N_sims)) as pool:
-            pool.map(integrate, self.RUN_PARAMS)
-        os.chdir(self.pdir)
+def run_Nbody(#.... params):
+    #take params and handoff to rebound
 
 class NbodySet(SimSet):
+    
     def __call__(self, params):
-        h = np.float64(params[0])
-        j = np.float64(params[1])
-        a0 = np.float64(params[2])
-        q = np.float64(params[3])
-        mu1 = np.float64(params[4])
-        T = np.float64(params[5])
-
-        Te_func = int(float(params[18]))
-        if Te_func:
-            Te1 = params[6]
-            Te2 = params[7]
-            Tm1 = params[8]
-            Tm2 = params[9]
-        else:
-            Te1 = np.float64(params[6])
-            Te2 = np.float64(params[7])
-            Tm1 = np.float64(params[8])
-            Tm2 = np.float64(params[9])
-
-        e1_0 = np.float64(params[10])
-        e2_0 = np.float64(params[11])
-        e1d = np.float64(params[12])
-        e2d = np.float64(params[13])
-        alpha2_0 = np.float64(params[14])
-        name = params[15]
-        dirname = params[16]
-        cutoff = np.float64(params[17])
-        g1_0 = np.float64(params[19])
-        g2_0 = np.float64(params[20])
+        # h = params[0]
+        # ...
+        # ...
+        # etc
         filename   = f"{name}.npz"
         figname    = f"{name}.png"
         paramsname = f"params-{name}.txt"
@@ -70,10 +25,22 @@ class NbodySet(SimSet):
                         f"Tm2={Tm2:0.1e} Te2={Te2:0.1e}\n" \
                         r"$e_{1,d}$ = " + f"{e1d:0.3f} " \
                         r"$e_{2,d}$ = " + f"{e2d:0.3f}")
-        run_compmass(h, j, mu1, q, a0, alpha2_0, e1_0, e2_0,g1_0,
-                     g2_0, Tm1, Tm2, Te1, Te2, T, suptitle, dirname,
-                     filename, figname, paramsname,
-                     verbose=self.verbose, secular=self.secular,
-                     e1d=e1d, e2d=e2d, overwrite=self.overwrite,
-                     cutoff=cutoff, method=self.method,
-                     Te_func=Te_func)
+        run_Nbody(verbose=self.verbose, secular=self.secular,
+                  overwrite=self.overwrite, method=self.method)
+
+
+class NbodyMigTrapSeries(SimSeries):
+    @series_dir
+    def __call__(self, Nproc=8):
+        # change to series directory
+        N_sims = self.RUN_PARAMS.shape[0]
+        overwrite = not self.load
+        integrate = NbodySet(verbose=True,
+                             overwrite=overwrite,
+                             secular=True,
+                             method="RK45")
+        np.savez("RUN_PARAMS", self.RUN_PARAMS)
+        
+        with Pool(processes=min(Nproc, N_sims)) as pool:
+            pool.map(integrate, self.RUN_PARAMS)
+
