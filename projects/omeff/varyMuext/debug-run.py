e07cee98ff87b2f2a06f04311a0daf9d9fd72d32
jtlaune <jtlaune@gmail.com>
2022-02-09 00:57:27 -0500

    Write notebook to be exported to latex pdf for meeting

--- projects/omeff/varyMuext/debug-run.py
+++ projects/omeff/varyMuext/debug-run.py
@@ -3,7 +3,21 @@ import numpy as np
 sys.path.append("/home/jtlaune/multi-planet-architecture/")
 import mpa
 from mpa.run import run_compmass_omeff
+from mpa.series import FOomEffSeries
+
+
+cwdpath = os.path.abspath(os.getcwd())
+projpath, _ = os.path.split(cwdpath)
+print(_)
+seriesname = "debug"
+seriesdir = os.path.join(projpath, seriesname)
+paramsname = seriesname+"-params.py"
+runpath = seriesdir
+# generate RUN_PARAMS from debug-params.py
+series = FOomEffSeries(seriesname, runpath, load=False)
+
 RUN_PARAMS = np.load("RUN_PARAMS.npz")
+# turn RUN_PARAMS into a list to expand in the fn argument
 RUN_PARAMS = RUN_PARAMS.f.arr_0
 params = []
 for par in RUN_PARAMS[0]:
--- projects/omeff/varyMuext/varyMuext-run.py
+++ projects/omeff/varyMuext/varyMuext-run.py
@@ -8,13 +8,10 @@ from mpa.series import FOomEffSeries
 # get run dir information
 # seriesname is simulation "series"
 
-# for symlink execution
-#abspath, filename = os.path.split(os.path.abspath(os.readlink(__file__)))
-#seriesname = os.path.basename(filename).split("-")[0]
-
 cwdpath = os.path.abspath(os.getcwd())
-projpath, _ = os.path.split(cwdpath)
-print(_)
+#projpath, _ = os.path.split(cwdpath)
+projpath = cwdpath
+
 seriesname = "varyMuext"
 seriesdir = os.path.join(projpath, seriesname)
 paramsname = seriesname+"-params.py"
